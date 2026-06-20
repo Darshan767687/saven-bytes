01341637af1dfcf7bb26ab4ecb2f5c66af962f29
Darshan <darshanbvdarshanbv0@gmail.com>
2026-06-20 22:30:00 +0530

    Move to Tomcat 10

--- src/main/java/com/saven/connection/ConnectionClass.java
+++ src/main/java/com/saven/connection/ConnectionClass.java
@@ -6,23 +6,34 @@ import java.sql.DriverManager;
 public class ConnectionClass {
 
     public static Connection getConnection() {
+
         Connection con = null;
+
         try {
+
+            // Load MySQL Driver
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            String url = System.getenv("MYSQL_URL");
+            String host = System.getenv("MYSQLHOST");
+            String port = System.getenv("MYSQLPORT");
+            String database = System.getenv("MYSQLDATABASE");
             String user = System.getenv("MYSQLUSER");
             String pass = System.getenv("MYSQLPASSWORD");
 
-            // DEBUG (remove later)
+            String url = "jdbc:mysql://" + host + ":" + port + "/" + database;
+
             System.out.println("DB URL: " + url);
             System.out.println("DB USER: " + user);
 
             con = DriverManager.getConnection(url, user, pass);
 
+            System.out.println("DATABASE CONNECTED SUCCESSFULLY");
+
         } catch (Exception e) {
+            System.out.println("DATABASE CONNECTION FAILED");
             e.printStackTrace();
         }
+
         return con;
     }
 }
